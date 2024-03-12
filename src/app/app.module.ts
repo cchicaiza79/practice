5eb8b1411d14b06b30817786ad51e2da2f2c72cb
cchicaiza79 <cchicaiza@conecta.com.ec>
2024-03-12 11:16:12 -0500

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
